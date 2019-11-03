d4cc08e3da3ddddc90d3d8619aa5a553b6900b2b
kuntalakrishna <kuntala.krishna@gmail.com>
2019-11-03 02:44:53 +0000

    Adde python script to upload updated files to S3 bucket

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,17 +1,21 @@
 import boto3
 from botocore.client import Config
 import StringIO
+import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-build_bucket = s3.Bucket('portfoliobuild.futurecreations.net')
-portfolio_bucket = s3.Bucket('portfolio.futurecreations.net')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-with zipfile.Zipfile(portfolio_zip) as myzip:
-    for name in myzip.namelist():
-        obj = myzip.open(name)
-        portfolio_bucket.upload(obj, name, ExtraArgs={'Content-Type': mimetypes.guess_type(name)[0]})
-        portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    build_bucket = s3.Bucket('portfoliobuild.futurecreations.net')
+    portfolio_bucket = s3.Bucket('portfolio.futurecreations.net')
+    
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for name in myzip.namelist():
+            obj = myzip.open(name)
+            portfolio_bucket.upload_fileobj(obj, name, ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+            portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+    return 'Hello from deployment lambda . . .'
